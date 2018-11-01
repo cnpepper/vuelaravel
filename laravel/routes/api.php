d87592ec78ec998c64da0952c5808b7467ea5ee2
刘建均 <liujianjun@liujianjundeAir.lan>
2018-11-01 22:16:18 +0800

    调整了一下路由

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -27,21 +27,20 @@ $api->version('v1', function ($api) {
 
         // 设置模块
         $api->prefix('setting')->group(function(){
-
             // 用户权限设置
             $api->prefix('permission')->group(function(){
-                $api->post('create','App\Http\Controllers\Api\Permission\CreateController@index');
-                $api->post('give','App\Http\Controllers\Api\Permission\GiveController@index');
-                $api->post('get','App\Http\Controllers\Api\Permission\GetController@index');
+                $api->post('create','App\Http\Controllers\Api\Setting\Permission\CreateController@index');
+                $api->post('give','App\Http\Controllers\Api\Setting\Permission\GiveController@index');
+                $api->post('get','App\Http\Controllers\Api\Setting\Permission\GetController@index');
             });
         });
 
         // 工作台模块
         $api->prefix('workspace')->group(function(){
             $api->prefix('mysql')->group(function(){
-                $api->post('create','App\Http\Controllers\Api\Mysql\CreateController@index');
-                $api->post('query','App\Http\Controllers\Api\Mysql\QueryController@index');
-                $api->post('check_query','App\Http\Controllers\Api\Mysql\CheckQueryController@index');
+                $api->post('create','App\Http\Controllers\Api\WorkSpace\Mysql\CreateController@index');
+                $api->post('query','App\Http\Controllers\Api\WorkSpace\Mysql\QueryController@index');
+                $api->post('check_query','App\Http\Controllers\Api\WorkSpace\Mysql\CheckQueryController@index');
             });
         });
 
